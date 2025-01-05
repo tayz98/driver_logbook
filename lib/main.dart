5320346eb52160ed5b60cd708db6c0bb2fb45ea0
Alexander Nachtigal <a.nachtigal98@gmail.com>
2025-01-05 22:22:10 +0100

    basic notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'dart:async';
+import 'package:elogbook/notification_configuration.dart';
 import 'package:elogbook/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import "../services/custom_bluetooth_service.dart";
 
-void main() {
+final StreamController<NotificationResponse> selectNotificationStream =
+    StreamController<NotificationResponse>.broadcast();
+
+const MethodChannel platform =
+    MethodChannel('dexterx.dev/flutter_local_notifications_example');
+
+const String portName = 'notification_send_port';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeNotifications();
+
+  requestNotificationPermission();
+
   FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
+  CustomBluetoothService customBluetoothService = CustomBluetoothService();
   runApp(const MyApp());
 }
 
---
+++ lib/services/notification_service.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+int id = 0;
+
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+    this.data,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+  final Map<String, dynamic>? data;
+}
+
+const String portName = 'notification_send_port';
+
+const String darwinNotificationCategoryText = 'textCategory';
+const String darwinNotificationCategoryPlain = 'plainCategory';
+const String navigationActionId = 'id_3';
+
+const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+final DarwinInitializationSettings initializationSettingsDarwin =
+    DarwinInitializationSettings(
+  // ...
+  notificationCategories: [
+    DarwinNotificationCategory(
+      'demoCategory',
+      actions: <DarwinNotificationAction>[
+        DarwinNotificationAction.plain('id_1', 'Action 1'),
+        DarwinNotificationAction.plain(
+          'id_2',
+          'Action 2',
+          options: <DarwinNotificationActionOption>{
+            DarwinNotificationActionOption.destructive,
+          },
+        ),
+        DarwinNotificationAction.plain(
+          'id_3',
+          'Action 3',
+          options: <DarwinNotificationActionOption>{
+            DarwinNotificationActionOption.foreground,
+          },
+        ),
+      ],
+      options: <DarwinNotificationCategoryOption>{
+        DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
+      },
+    )
+  ],
+);
+
+final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid, iOS: initializationSettingsDarwin);
